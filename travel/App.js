83e44249283c44c3985645a6beac4a4dc77a0ce3
Nathan Dias <nathanfaria9@gmail.com>
2022-11-16 19:33:24 -0300

    Login: Ideia pessoal

---
+++ travel/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { NavigationContainer } from '@react-navigation/native';
+import Routes from './src/routes';
+
+import GeneralStatusBarColor from './src/components/GeneralStatusBarColor';
+
+export default function App() {
+  return (
+      <NavigationContainer>
+        <GeneralStatusBarColor backgroundColor="#003580" barStyle="light-content"/>
+        <Routes />
+      </NavigationContainer>
+  );
+}
---
+++ travel/src/pages/Welcome/index.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+
+import * as Animatable from 'react-native-animatable'; 
+
+import { useNavigation } from '@react-navigation/native';
+
+export default function Welcome() {
+  const navigation = useNavigation();
+
+ return (
+   <View style={styles.container}>
+      <View style={styles.containerLogo}>
+        <Animatable.Image animation='flipInY' source={require('../../assets/logo.png')} style={{width: '140%'}} resizeMode="contain"/>
+      </View>
+      <Animatable.View delay={700} animation='fadeInUp' style={styles.containerForm}>
+        <Text style={styles.title}>Encontre sua próxima estadia, com o Booking.com</Text>
+        <Text style={styles.text}>Faça o login para começar</Text>
+
+        <TouchableOpacity style={styles.button} onPress={ () => navigation.navigate('SignIn')}>
+          <Text style={styles.buttonText}>Acessar</Text>
+        </TouchableOpacity>
+      </Animatable.View>
+   </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    backgroundColor: '#003580'
+  },
+  containerLogo:{
+    flex: 2,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  containerForm:{
+    flex: 1,
+    backgroundColor: '#FFF',
+    borderTopRightRadius: 25,
+    borderTopLeftRadius: 25,
+    paddingStart: '5%',
+    paddingEnd: '5%',
+  },
+  title:{
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 28,
+    marginBottom: 12,
+  },
+  text:{
+    color: '#a1a1a1',
+  },
+  button:{
+    position: 'absolute',
+    backgroundColor: '#003580',
+    borderRadius: 50,
+    paddingVertical: 8,
+    width: '60%',
+    alignSelf: 'center',
+    bottom: '15%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonText:{
+    fontSize: 18,
+    color: '#FFF',
+    fontWeight: 'bold',
+  }
+})
